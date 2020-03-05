b081023e04eed792a7062c4ef353d75c1b07a850
Benjamin Howarth <me@benjaminhowarth.ltd>
2020-03-05 14:39:31 +0000

    Switched DB execution over to PetaPoco instead of DataReader & SqlCommand. Also inserted a test to check if running on SQLCE, if so, run statements separately, if not, bulk them into a single execution.

--- src/Our.Umbraco.UnVersion/Services/UnVersionService.cs
+++ src/Our.Umbraco.UnVersion/Services/UnVersionService.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Data.SqlServerCe;
+using System.Linq;
+using System.Net.Mime;
 using System.Reflection;
 using log4net;
 using umbraco;
 using Umbraco.Core;
 using Umbraco.Core.Models;
-using Umbraco.Web;
+using Umbraco.Core.Persistence;
 
 namespace Our.Umbraco.UnVersion.Services
 {
@@ -19,7 +22,7 @@ namespace Our.Umbraco.UnVersion.Services
 
         private readonly IUnVersionConfig _config;
         private readonly bool _catchSqlExceptions;
-        private readonly string _ceStatementEnd = string.Format("{0}GO{0}", Environment.NewLine);
+        private readonly string _ceStatementEnd = $"{Environment.NewLine}GO{Environment.NewLine}";
 
         public UnVersionService(IUnVersionConfig config, bool catchSqlExceptions)
         {
@@ -37,7 +40,7 @@ namespace Our.Umbraco.UnVersion.Services
             if (_config.ConfigEntries.ContainsKey("$_ALL"))
                 configEntries.AddRange(_config.ConfigEntries["$_ALL"]);
 
-            if (configEntries.Count <= 0)
+            if (!configEntries.Any())
             {
                 if (Logger.IsDebugEnabled)
                     Logger.Debug("No unversion configuration found for type " + content.ContentType.Alias);
@@ -47,128 +50,77 @@ namespace Our.Umbraco.UnVersion.Services
 
             foreach (var configEntry in configEntries)
             {
-	            var isValid = true;
+                var isValid = true;
 
-	            if (!String.IsNullOrEmpty(configEntry.RootXPath))
-	            {
-		            if (content.Level > 1 && content.Parent() != null)
-		            {
-			            var ids = GetNodeIdsFromXpath(configEntry.RootXPath);
-			            isValid = ids.Contains(content.ParentId);
-		            }
-	            }
+                if (!String.IsNullOrEmpty(configEntry.RootXPath))
+                {
+                    if (content.Level > 1 && content.Parent() != null)
+                    {
+                        var ids = GetNodeIdsFromXpath(configEntry.RootXPath);
+                        isValid = ids.Contains(content.ParentId);
+                    }
+                }
 
-	            if (!isValid)
-		            continue;
+                if (!isValid)
+                    continue;
 
-	            var connStr = ConfigurationManager.ConnectionStrings["umbracoDbDSN"];
+                var versionsToKeep = VersionsToKeep(content.Id, configEntry).Select(x => $"'{x.ToString("D")}'");
+                var versionsToKeepString = string.Join(",", versionsToKeep);
 
-	            using (var conn = connStr.ProviderName.Contains("SqlServerCe")
-		            ? (IDbConnection) new SqlCeConnection(connStr.ConnectionString)
-		            : (IDbConnection) new SqlConnection(connStr.ConnectionString))
-	            {
+                if (Logger.IsDebugEnabled)
+                    Logger.Debug("Keeping versions " + versionsToKeepString);
 
-		            conn.Open();
-		            var vesionsToKeep = VersionsToKeep(content.Id, configEntry, conn);
-		            var versionsToKeepString = string.Join(",", vesionsToKeep);
+                var commands = new List<string>() {
+                    @"DELETE FROM cmsPreviewXml WHERE nodeId = {0} AND versionId NOT IN ({1});",
+                    @"DELETE FROM cmsPropertyData WHERE contentNodeId = {0} AND versionId NOT IN ({1});",
+                    @"DELETE FROM cmsContentVersion WHERE contentId = {0} AND versionId NOT IN ({1});",
+                    @"DELETE FROM cmsDocument WHERE nodeId = {0} AND versionId NOT IN ({1});",
+                };
 
-		            if (Logger.IsDebugEnabled)
-			            Logger.Debug("Keeping versions " + versionsToKeepString);
-
-		            var sqlString = string.Format(
-			            string.Concat(
-				            @"DELETE FROM cmsPreviewXml WHERE nodeId = {0} AND versionId NOT IN ({1});",
-				            _ceStatementEnd,
-				            @"DELETE FROM cmsPropertyData WHERE contentNodeId = {0} AND versionId NOT IN ({1});",
-				            _ceStatementEnd,
-				            @"DELETE FROM cmsContentVersion WHERE contentId = {0} AND versionId NOT IN ({1});",
-				            _ceStatementEnd,
-				            @"DELETE FROM cmsDocument WHERE nodeId = {0} AND versionId NOT IN ({1});"),
-			            content.Id,
-			            versionsToKeepString
-		            );
-
-		            ExecuteSql(sqlString, conn);
-                    conn.Close();
-	            }
+                if (ApplicationContext.Current.DatabaseContext.DatabaseProvider == DatabaseProviders.SqlServerCE) {
+                    foreach (var command in commands) {
+                        var formattedCmd = string.Format(command, content.Id, versionsToKeepString);
+                        ApplicationContext.Current.DatabaseContext.Database.Execute(formattedCmd);
+                    }
+                } else {
+                    // BH: Every other provider supports multiple statements per execution, this enables bulk execution.
+                    var bulkCommand = string.Format(string.Join(string.Empty, commands), content.Id, versionsToKeepString);
+                    ApplicationContext.Current.DatabaseContext.Database.Execute(bulkCommand);
+                }
             }
         }
 
-        void ExecuteSql(string sql, IDbConnection connection)
-        {
-            if (Logger.IsDebugEnabled)
-                Logger.Debug(sql);
-
-            var command = connection.CreateCommand();
-            command.CommandType = CommandType.Text;
-            command.CommandText = sql;
-
-            if (_catchSqlExceptions)
-            {
-                try
-                {
-                    command.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
-                    Logger.Warn("Executing " + sql, ex);
-                }
-            }
-            else
-            {
-                command.ExecuteNonQuery();
-            }
-        }
-
-        private IEnumerable<string> VersionsToKeep(int contentId, UnVersionConfigEntry configEntry, IDbConnection connection)
+        private IEnumerable<Guid> VersionsToKeep(int contentId, UnVersionConfigEntry configEntry)
         {
             // Get a list of all versions
             // TODO: Need to find a better way to do this, but SQL CE 4 doesn't allow sub queries
-
-            var sql = string.Format(@"SELECT			
+            var sql = string.Format(@"SELECT            
                                     cv.VersionId,
-	                                cv.VersionDate,
-	                                d.published,
-	                                d.newest
-                FROM				cmsContentVersion cv
-                LEFT OUTER JOIN		cmsDocument d ON d.versionId = cv.VersionId
-                WHERE				cv.ContentId = {0}
+                                    cv.VersionDate,
+                                    d.published,
+                                    d.newest
+                FROM                cmsContentVersion cv
+                LEFT OUTER JOIN        cmsDocument d ON d.versionId = cv.VersionId
+                WHERE                cv.ContentId = {0}
                 ORDER BY            cv.VersionDate DESC",
                 contentId);
 
             if (Logger.IsDebugEnabled)
                 Logger.Debug(sql);
 
-            // var results = ApplicationContext.Current.DatabaseContext.Database.Fetch<>();
-
-            var command = connection.CreateCommand();
-            command.CommandType = CommandType.Text;
-            command.CommandText = sql;
-
-            var versionsToKeep = new List<string>();
-            var readerIndex = 0;
+            var versionsToKeep = new List<Guid>();
 
             try
             {
-                using (var reader = command.ExecuteReader())
-		    {
-			    while (reader.Read())
-			    {
-				    var versionId = reader.GetGuid(0);
-				    var versionDate = reader.GetDateTime(1);
-				    var published = !reader.IsDBNull(2) && reader.GetBoolean(2);
-				    var newest = !reader.IsDBNull(3) && reader.GetBoolean(3);
+                var versions = ApplicationContext.Current.DatabaseContext.Database.Fetch<VersionsModel>(sql);
+                var readerIndex = 0;
+                foreach (var version in versions) {
+                    var daysDiff = (DateTime.Now - version.VersionDate).Days;
+                    if (version.Published || version.Newest || (daysDiff < configEntry.MaxDays && readerIndex <= configEntry.MaxCount))
+                        versionsToKeep.Add(version.VersionID);
 
-				    readerIndex++;
-
-				    var daysDiff = (DateTime.Now - versionDate).Days;
-				    if (published || newest || (daysDiff < configEntry.MaxDays && readerIndex <= configEntry.MaxCount))
-					    versionsToKeep.Add("'" + versionId.ToString("D") + "'");
-			    }
-
-			    reader.Close();
-			    reader.Dispose();
-		    }
+                    readerIndex++;
+                }
             }
             catch (Exception ex)
             {
---
+++ src/Our.Umbraco.UnVersion/VersionsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Our.Umbraco.UnVersion
+{
+    public class VersionsModel
+    {
+        public Guid VersionID { get; set; }
+        public DateTime VersionDate { get; set; }
+        public bool Published { get; set; }
+        public bool Newest { get; set; }
+    }
+}
