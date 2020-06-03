1a5a6c9808b9e7c376c4f562166bb8f1ae6c881a
Søren Kottal <sk@ecreo.dk>
2020-06-03 21:33:50 +0200

    added some more logging

--- src/Our.Umbraco.UnVersion/Services/UnVersionService.cs
+++ src/Our.Umbraco.UnVersion/Services/UnVersionService.cs
@@ -36,7 +36,7 @@ namespace Our.Umbraco.UnVersion.Services
 
             if (configEntries.Count <= 0)
             {
-                _logger.Debug<UnVersionService>("No unversion configuration found for type " + content.ContentType.Alias);
+                _logger.Debug<UnVersionService>("No unversion configuration found for type {alias}", content.ContentType.Alias);
                 return;
             }
 
@@ -56,17 +56,24 @@ namespace Our.Umbraco.UnVersion.Services
                 }
 
                 if (!isValid)
+                {
+                    _logger.Debug<UnVersionService>("Configuration invalid, rootXPath must be {rootXPath}", configEntry.RootXPath);
                     continue;
+                }
 
                 var allVersions = _contentService.GetVersionsSlim(content.Id, 0, int.MaxValue).ToList();
 
                 if (!allVersions.Any())
+                {
+                    _logger.Debug<UnVersionService>("No versions of content {contentId} found", content.Id);
                     continue;
+                }
 
                 var versionIdsToDelete = GetVersionsToDelete(allVersions, configEntry, DateTime.Now);
 
                 foreach (var vid in versionIdsToDelete)
                 {
+                    _logger.Debug<UnVersionService>("Deleting version {versionId} of content {contentId}", vid, content.Id);
                     _contentService.DeleteVersion(content.Id, vid, false);
                 }
 
@@ -87,13 +94,18 @@ namespace Our.Umbraco.UnVersion.Services
 
             int iterationCount = 0;
 
+
+            _logger.Debug<UnVersionService>("Getting versions for config entry. {alias}, {maxCount}, {maxDays}, {rootXpath}", configEntry.DocTypeAlias, configEntry.MaxCount, configEntry.MaxDays, configEntry.RootXPath);
+
             foreach (var version in versions)
             {
                 iterationCount++;
+                _logger.Debug<UnVersionService>("Comparing version {versionId}, iterationCount is {iterationCount}", version.VersionId, iterationCount);
 
                 // If we have a maxCount and the current iteration is above that max-count
                 if (configEntry.MaxCount > 0 && iterationCount > configEntry.MaxCount)
                 {
+                    _logger.Debug<UnVersionService>("Remove version {versionId}, because iterationCount is {iterationCount} and max count is {maxCount}", version.VersionId, iterationCount, configEntry.MaxCount);
                     versionIdsToDelete.Add(version.VersionId);
                     // no need to compare dates since we've already added this version for deletion
                     continue;
@@ -105,6 +117,7 @@ namespace Our.Umbraco.UnVersion.Services
                     var dateRemoveBefore = currentDateTime.AddDays(0 - configEntry.MaxDays);
                     if (version.UpdateDate < dateRemoveBefore)
                     {
+                        _logger.Debug<UnVersionService>("Remove version {versionId}, because version is updated {updateDate} and max days is {maxDays} (cutoff: {dateRemoveBefore})", version.VersionId, version.UpdateDate, configEntry.MaxDays, dateRemoveBefore);
                         versionIdsToDelete.Add(version.VersionId);
                     }
                 }
